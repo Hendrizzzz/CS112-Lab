5003cab20157c7da4e11ad91ffe6ff0a38d6666a
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2023-09-09 18:38:29 +0800

    2nd Commit

--- RightTriangle3.java
+++ RightTriangle3.java
@@ -1,3 +1,29 @@
+/*
+Name: Jim Hendrix T. Bag-eo
+Programming Date: 9 /8/2023
+Activity Name and Number: Prelim Programming Exercise 4
+Problem:
+ Write a program that computes and shows the hypotenuse, perimeter, and the area of a right triangle with
+ a given base and height. Let the base and height be entered through the keyboard at program runtime.
+Analysis:
+Input: base and height of the right triangle
+Processes:
+Read the base and height of the right triangle
+Compute the hypotenuse of the right triangle
+Compute the perimeter of the right triangle
+Compute the area of the right triangle
+Display results
+Output: perimeter, area
+Algorithm:
+1. Read/Accept the width and length of the rectangle
+2. Compute for the hypotenuse in a different method: Math.sqrt(base * base + height * height)
+3. Compute for the perimeter in a different method: 2 * ( width + length)
+4. Compute for the area in a different method: width * length
+5. Show the hypotenuse of the right triangle
+6. Show the perimeter of the right triangle
+7. Show the area of the right triangle
+*/
+
 package exercises.prelims;
 
 import java.util.Scanner;
@@ -45,6 +71,7 @@ public class RightTriangle3 {
         //returning the computed value for perimeter of the right triangle
         return Math.sqrt(base * base + height * height);
     }//end of the computePerimeter method
+
 
     static double computePerimeter(double base1, double height1, double hypotenuse1){
         //returning the computed value for perimeter of the right triangle
