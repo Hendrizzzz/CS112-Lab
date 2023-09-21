00bf5c2eed0e397a3c2586648f532f67660ddc76
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2023-09-21 19:00:39 +0800

    Try Catch update

--- Circle.java
+++ Circle.java
@@ -1,5 +1,3 @@
-package exercises.prelims;
-
 /*
 * Name: Bag-eo, Jim Hendrix T.
 * Programming Date: 8/25/23
@@ -7,10 +5,11 @@ package exercises.prelims;
 
 -----------------------------------------------------------------
 Input: Radius of a circle (radius)
-Processes: Compute the circumference of the circle
- Compute the area of the circle
- Display results
-Output: circumference, area
+Processes:
+    Compute the circumference of the circle
+    Compute the area of the circle
+    Display results
+    Output: circumference, area
 ------------------------------------------------------------------
 Algorithm:
 1. Assign the radius of the circle
@@ -22,42 +21,62 @@ Algorithm:
  -------------------------------------------------------------------
 */
 
-import java.util.Scanner;                 //importing a scanner
+package exercises.prelims;
+
+import java.util.Scanner;
 
 public class Circle {
     public static void main(String[] args) {
 
-
-        float radius;                                                   //declaring initializing variables
-        double circumference;
-        double areaOfCircle;
-
+        // Greet the user
         System.out.println("Welcome to the Circle Calculator!");
 
-        Scanner input = new Scanner (System.in);                          //creating a scanner method named 'input'
+        // Declare and initialize variables
+        double radius;
+        double circumference;
+        double areaOfCircle;
+        boolean isValidRadius = true;
 
-        System.out.print("Please, enter the radius of the circle: ");     //storing a value inputted by the user to radius
-        radius = input.nextInt();
+        // Create a scanner object named 'input' for user input
+        Scanner input = new Scanner(System.in);
+
+        do {
+            try {
+                // Prompt the user to enter the radius of the circle
+                System.out.print("Please, enter the radius of the circle: ");
+                radius = input.nextDouble();
+
+                // Input validation: Check if radius is negative, if so, ask for input again
+                if (radius < 0) {
+                    System.out.println("Invalid input. Radius cannot be negative. Please try again.\n");
+                    input.nextLine(); // Consume the invalid input
+                    continue; // Go back to the beginning of the loop
+                }
+
+                // Calculate the circumference and area
+                circumference = 2 * Math.PI * radius;
+                areaOfCircle = Math.PI * radius * radius;
+
+                // Display the results
+                System.out.println("\n**********************************************************");
+                System.out.println("*");
+                System.out.println("*");
+                System.out.println("* Radius of circle is " + radius + " *");
+                System.out.println("* Circumference of circle is " + circumference + " *");
+                System.out.println("* Area of circle is " + areaOfCircle + " *");
+                System.out.println("*");
+                System.out.println("*");
+                System.out.print("**********************************************************");
+
+                isValidRadius = false; // Set checker to false to exit the loop
+            } catch (java.util.InputMismatchException e) {
+                // Handles invalid input with an error message
+                System.out.println("Invalid input. Please enter a valid number for the radius.\n");
+                input.nextLine(); // Consume the invalid input
+            }
+        } while (isValidRadius); // Continue the loop until checker is false
+
+        // Close the input scanner
         input.close();
-
-        circumference = 2 * 3.1416 * radius;                               //formula for finding the circumference of  a circle
-
-        areaOfCircle = 3.1416 * radius * radius;                          //formula for the area of a circle
-
-//output layout
-        System.out.println();
-        System.out.println();
-        System.out.println(" **********************************************************");
-        System.out.println(" * *");
-        System.out.println(" * *");
-        System.out.println(" * Radius of circle is " + radius +" *");
-        System.out.println(" * Circumference of circle is " + circumference + " *");
-        System.out.println(" * Area of circle is " + areaOfCircle +" *");
-        System.out.println(" * *");
-        System.out.println(" * *");
-        System.out.println(" **********************************************************");
-    } // end of main method
-} // end of class
-
-
-
+    }
+}
