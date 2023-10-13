ec8d96e36eba035d90542cca15db6b74613ff219
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2023-10-13 12:16:44 +0800

    Update NestedForLoop(Triangle).java

--- NestedForLoop(Triangle).java
+++ NestedForLoop(Triangle).java
@@ -1,21 +1,21 @@
 public class Haha {
     public static void main(String[] args) {
 
-        for (int row = 1; row <= 5; row++){ //outer loop for row
+        for (int row = 1; row <= 5; row++){ //outer for-loop for row
                                             //iterates until 5th row
 
 
-                for (int space = 1; space <= 5 - row; space++){ //inner loop for left aligned num
+                for (int space = 1; space <= 5 - row; space++){ //inner for-loop to print spaces
                         System.out.print("  ");  //Double space
                 }
 
 
-                for (int num = 1; num<= row; num++){ //inner loop to print number per row
-                        System.out.print(" " + num);
+                for (int num = 1; num<= row; num++){ //inner for-loop to print number per row
+                        System.out.print(num + " "); 
                 }
 
 
-            System.out.println(); //prints for next row/line
+            System.out.println(); //prints nextline
 
         }  // end of outer loop
 
