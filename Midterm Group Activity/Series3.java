3738a8ae7d0f7124e5f5c5e072597570f73ab6b4
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2023-10-20 13:24:04 +0800

    Update Series3.java

--- Midterm Group Activity/Series3.java
+++ Midterm Group Activity/Series3.java
@@ -1,19 +1,19 @@
-public class Haha {
+package exercises.midterms;
+public class NestedForLoopSeries3 {
     public static void main(String[] args) {
 
-        for (int row = 1; row <= 5; row++){ //outer for-loop for row
-                                            //iterates until 5th row
+        int number = 5; //Number of lines
 
+        for (int row = 1; row <= number; row++){ //outer for-loop for row
+            //iterates until 5th row
 
-                for (int space = 1; space <= 5 - row; space++){ //inner for-loop to print spaces
-                        System.out.print("  ");  //Double space
-                }
+            for (int space = 1; space <= number - row; space++){ //inner for-loop to print spaces
+                System.out.print("   ");  //3 spaces
+            }
 
-
-                for (int num = 1; num<= row; num++){ //inner for-loop to print number per row
-                        System.out.print(num + " "); 
-                }
-
+            for (int num = 1; num <= row; num++){ //inner for-loop to print number per row
+                System.out.print(num + "  "); //2 spaces
+            }
 
             System.out.println(); //prints nextline
 
